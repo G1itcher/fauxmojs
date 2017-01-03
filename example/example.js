bebeebb58696186700a38259c671796557329962
Liam Jones <liamaj@gmail.com>
2017-01-03 21:28:53 +0000

    Fixed FauxMo implementation

--- example/example.js
+++ example/example.js
@@ -1,24 +1,29 @@
 'use strict';
 
-const FauxMo = require('../lib');
+const Server = require('../lib/index-Y');
+const FauxMo = require('../lib/devices/FauxMo');
+const FauxHue = require('../lib/devices/FauxHue');
 
-let fauxMo = new FauxMo(
+let server = new Server(
   {
     devices: [
+      new FauxMo("test light", 11002, (action) => {
+          console.log('test light action:', action);
+        }
+      ),
+      new FauxMo("test fan", 11003, (action) => {
+          console.log('test fan action:', action);
+        }
+      )
+      /*
       {
-        name: 'office light',
-        port: 11000,
+        name: 'test light',
+        port: 11002,
         handler: (action) => {
           console.log('office light action:', action);
         }
-      },
-      {
-        name: 'office fan',
-        port: 11001,
-        handler: (action) => {
-          console.log('office fan action:', action);
-        }
       }
+*/
     ]
   });
 
